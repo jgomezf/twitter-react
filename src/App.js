ec233cc0b3a1b326c11500de6fc642362537380c
Juan David Gomez <fogodaju@gmail.com>
2021-06-22 22:39:01 -0500

    Import SvgIcon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { createMuiTheme, SvgIcon, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@material-ui/core/Container';
@@ -39,6 +40,7 @@ function HomeIcon(props) {
     </SvgIcon>
   );
 }
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -68,7 +70,7 @@ function App() {
             >
               <MenuIcon />
             </IconButton>
-            <HomeIcon/>
+            <HomeIcon />
             <ListItem component={NavLink} to="/" button>
               <Typography variant="h6" className={classes.title}>
                 React Twitter
